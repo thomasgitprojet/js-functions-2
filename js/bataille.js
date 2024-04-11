485b3487db6e26bb5f43b8e7b2ed4d3c7f1ff681
caravane <thomasdesse.pro@gmail.com>
2024-04-11 09:33:30 +0200

    oups j ai oublié de faire des commits

---
+++ js/bataille.js
@@ -0,0 +1,221 @@
+/**
+ * Return a random value between 0 and a chosen number.
+ * @param {number} max - chosen number
+ * @returns {number} - random value
+ */
+function getRandomValue(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+/**
+ * return a random value between 0 and a chosen number.
+ * @param {number} max - chosen number
+ * @returns {number} - random value
+ */
+function getRandomValue(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+
+/**
+ * Gets a random value from an array
+ * @param {array} array of datas 
+ * @returns {*} - a random value
+ */
+function getRandomArrayValue(array) {
+    return array[getRandomValue(array.length - 1)];
+
+}
+
+/**
+ * Gets a random value from an array
+ * @param {array} array of datas 
+ * @returns {*} - a random value
+ */
+function getRandomArrayValue(array) {
+    return array[getRandomValue(array.length - 1)];
+
+}
+
+/**
+ * return a random value between 0 and a chosen number.
+ * @param {number} max - chosen number
+ * @returns {number} - random value
+ */
+function getRandomValue(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+let players = [
+
+    {
+        playerName : 'Samir',
+        life : true,
+        pv : 50,
+        xp : 5,
+        poweratk : 8,
+        powerdef : 5,
+        damage : 0
+    },
+
+    {
+        playerName : 'Paul',
+        life : true,
+        pv : 50,
+        xp : 4,
+        poweratk : 3,
+        powerdef : 6,
+        damage : 0
+    },
+
+    {
+        playerName : 'Loanne',
+        life : true,
+        pv : 50,
+        xp : 6,
+        poweratk : 6,
+        powerdef : 7,
+        damage : 0
+    },
+
+    {
+        playerName : 'Tom',
+        life : true,
+        pv : 50,
+        xp : 7,
+        poweratk : 4,
+        powerdef : 10,
+        damage : 0
+    }
+]
+
+///////////////////////////////////////calc pv///////////////////////
+
+function calcPv(playerPv, playerDamage) {
+    return playerPv - playerDamage
+}
+
+//////////////////////////////////function attack//////////////////////
+
+function getAttackScore(attacker) {
+    return getRandomValue(attacker.poweratk) + attacker.xp;
+}
+
+// function attack (player) {
+//     return player.xp + Math.round(player.poweratk * Math.random())
+// }
+
+////////////////////////////////function defence//////////////////////
+function getDefenceScore(defender) {
+    return getRandomValue(defender.powerdef) + defender.xp;
+}
+
+// function defence (player) {
+//     return player.xp + Math.round(player.powerdef * Math.random())
+// }
+
+///////////////////////////////////////////out player////////////////////
+
+function outPlayer (array) {
+    let player;
+    for (player of players) {
+       //  console.log(player);
+        if (player.pv <= 0) {
+           player.life = false
+        }
+    }
+   array = array.filter((value) => value.life === true)
+   return array
+}
+
+
+
+////////////////////////////////is winner/////////////////////////////////
+
+function isWinner (array) {
+
+    if (array.length == 1) {
+        console.log(`game over le gagnant est ${array[0].playerName}`);
+    }
+}
+
+
+/////////////////////////////challengers/////////////////////////////
+
+function getChallengers (array) {
+    let playersBattle = [];
+    let i = 0;
+
+while (i < 2) {
+    playersBattle.push(getRandomArrayValue(array));
+    if (playersBattle[0] === playersBattle[1]) {
+        playersBattle.pop();
+    }else {
+        i++;
+    }
+}
+return playersBattle;
+}
+
+//////////////////////////////round/////////////////////////
+
+// function getRound
+
+////////////////////////////function figth////////////////////////
+
+function figth (figther, defender) {
+    let attackScore = getAttackScore(figther);
+    let defenceScore = getDefenceScore(defender);
+
+    if (attackScore > defenceScore) {
+
+        console.log(`attaque : ${attackScore}`);
+        console.log(`défence : ${defenceScore}`);
+        console.log(`L'attaquant ${figther.playerName} win`);
+
+       defender.pv-= attackScore
+    }else {
+        console.log(`attaque : ${attackScore}`);
+        console.log(`défence : ${defenceScore}`);
+        console.log(`le défenseur ${defender.playerName} win`);
+
+       figther.pv-= defenceScore
+    }
+    // console.log(figther, defender);
+    // return
+}
+ //figth(players[0], players[1])
+ // figth(playersBattle[0], playersBattle[1])
+ // while (players.length)
+
+
+while (players.length > 1) {
+
+    let chall = getChallengers (players);
+        // console.log(chall);
+
+        figth(chall[0], chall[1]);
+       
+       players = outPlayer(players)
+        console.table(players);
+
+        isWinner(players)
+}
+
+
+
+
+
+    // if (players.length > 1) {
+
+    //     let chall = getChallengers (players);
+    //     console.log(chall);
+
+    //     figth(chall[0], chall[1]);
+       
+    //    players = outPlayer(players)
+    //     console.table(players);
+    // }
+    // console.log(getChallengers(players));
+    // console.log(outPlayer(players));
+    // console.log(outPlayer(players));
--- js/script.js
+++ js/script.js
@@ -5,80 +5,191 @@
 
 console.info("1/ Implémentez une fonction qui prend en paramètre les dimensions de 2 côtés d'un triangle rectangle et retourne la dimension de l'hypoténuse.");
 
-console.log();
-console.log();
+/**
+ * calculed hypotenus
+ * @param {number} nbr1 
+ * @param {number} nbr2 
+ * @returns {number} result of hypo
+ */
+function calcHypotenus (nbr1, nbr2) {
+    return Math.hypot(nbr1, nbr2)
+}
+console.log(calcHypotenus(3, 4));
+console.log(calcHypotenus(10, 14));
 
 /* ------------------------------------------------------*/
 
 console.info("2/ Implémentez une fonction qui retourne le nom et le prénom du plus âgé de 2 utilisateurs passés en paramètres parmi les 4 ci-dessous.");
+/**
+ * get last name and first name of user
+ * @param {object} usera 
+ * @param {object} userb 
+ * @returns {string} last name and first name
+ */
+function getFirstAndLastName (usera, userb) {
+    let userAName = `${usera.firstName} ${usera.lastName}`;
+    let userBName = `${userb.firstName} ${userb.lastName}`;
+    return usera.age > userb.age ? userAName : userBName
+}
 
 const user1 = {firstName: "Paul", lastName: "Rick", age: 35};
 const user2 = {firstName: "Samir", lastName: "Ah", age: 22};
 const user3 = {firstName: "Loanne", lastName: "Bourdin", age: 28};
 const user4 = {firstName: "Abdel", lastName: "Dems", age: 44};
 
-console.log();
-console.log();
+console.log(getFirstAndLastName(user1, user2));
+console.log(getFirstAndLastName(user4, user3));
 
 /* ------------------------------------------------------*/
 
 console.info("3/ Implémentez une fonction qui retourne la valeur la plus grande d'un tableau de nombres, quelqu'en soit la taille.");
-
-console.log();
-console.log();
+function maxValur (array) {
+    return Math.max(...array);
+}
+console.log(maxValur([25, 36, 2, 78]));
+console.log(maxValur([52, 19, 30, 4]));
 
 /* ------------------------------------------------------*/
 
 console.info("4/ Implémentez une fonction qui prend en paramètre un texte et retourne un objet comptant le nombre d'occurence de chaque mot.");
-
-console.log();
-console.log();
+function  countRepeatedOfOccurencesOfWords(txt) {
+    let words = {};
+    for (let word of txt.split(" ")) {
+        console.log(word);
+        if (words[word] === undefined || words[word] === 0) {
+            words[word] = 1;
+        }else {
+            words[word]++
+        }
+    }
+    return words
+}
+console.log(countRepeatedOfOccurencesOfWords("Implémentez une fonction qui prend en paramètre un texte et retourne un objet comptant le nombre d'occurence de chaque mot"));
 
 /* ------------------------------------------------------*/
 
 console.info("5/ Implémentez une fonction qui retourne une valeur entière alétoire comprise entre 2 valeurs passées en paramètres.");
-
-console.log();
+/**
+ * get random number from two param
+ * @param {number} a min
+ * @param {number} b max
+ * @returns {number}
+ */
+function getrandomNumberFromParam (a, b) {
+    return Math.floor(Math.random() * (b - a + 1))
++ a}
+console.log(getrandomNumberFromParam(5, 8));
 console.log();
 
 /* ------------------------------------------------------*/
 
 console.info("6/ Implémentez une fonction qui retourne une chaîne de caractère aléatoire composée de chiffres et de lettres en majuscule et minuscule, de la taille passée en paramètre.");
 
-console.log();
-console.log();
+/**
+ * return a random value between 0 and a chosen number.
+ * @param {number} max - chosen number
+ * @returns {number} - random value
+ */
+function getRandomValue(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+
+/**
+ * Gets a random value from an array
+ * @param {array} array of datas 
+ * @returns {*} - a random value
+ */
+function getRandomArrayValue(array) {
+    return array[getRandomValue(array.length - 1)];
+
+}
+
+function creatPassWord (length) {
+    let arrayChar = [];
+    let passWord = "";
+
+    for (let i = 48; i <= 57; i++) {
+        arrayChar.push(String.fromCharCode(i))
+    }
+
+    for (let i = 65; i <= 90; i++) {
+        arrayChar.push(String.fromCharCode(i))
+    }
+
+    for (let i = 97; i <= 122; i++) {
+        arrayChar.push(String.fromCharCode(i))
+    }
+
+    for (let j = 0; j < length; j++) {
+      passWord += getRandomArrayValue(arrayChar);
+    }
+
+    return passWord;
+
+}
+
+console.log(creatPassWord(8));
+console.log(creatPassWord(3));
 
 /* ------------------------------------------------------*/
 
 console.info("Pour les questions suivantes enrichissez l'objet 'newbie' ci-dessous.");
 
+
+/* ------------------------------------------------------*/
+
+/**
+ * Calculate an age from a birthdate.
+ * @param {string} birthdate birthdate in texte form ex. "1955-01-14"
+ * @returns {number} the age
+*/
+function calculateAgeFromBirthdate(birthdate) {
+    const b = new Date(birthdate);
+    
+    // Is the birthdate a valid date ?
+    if (!(b instanceof Date) || isNaN(b)) return;
+    
+    // birthdate is valid
+    return (new Date(Date.now() - b)).getFullYear() - 1970;
+}
 const newbie = {
     firstName: "Paul",
     lastName: "Bismuth",
     birthdate: "1995-02-14",
     job: "web developer",
     city: "Detroit",
-    skills: ["HTML", "CSS"]
+    skills: ["HTML", "CSS"],
+    age : function() {
+        return calculateAgeFromBirthdate(this.birthdate);       
+    },
+    showSkills : function () {
+        return `Paul est le boss niveau ${this.skills[0]} et ${this.skills[1]}`
+    },
+    addSkill : function (skill) {
+        this.skills.push(skill)
+    }
+
 };
 
-/* ------------------------------------------------------*/
+// newbie.age = calculateAgeFromBirthdate(newbie.birthdate);
 
 console.info("7/ Implémentez une méthode retournant l'âge de Paul.");
 
-console.log();
+console.log(newbie.age());
 
 /* ------------------------------------------------------*/
 
 console.info("8/ Implémentez une méthode retournant un texte listant les compétences de Paul séparées par des virgules.");
 
-console.log();
+console.log(newbie.showSkills());
 
 /* ------------------------------------------------------*/
 
 console.info("9/ Implémentez une méthode qui ajoute à Paul une compétence passée en paramètre.");
 console.info("Ajoutez la compétence Javascript et utilisez la méthode précédente pour vous assurer du résultat.");
-
-console.log();
+newbie.addSkill("js")
+console.log(newbie.skills);
 
 /* ------------------------------------------------------*/
 
